131c62d0e701959ca7e6182d978c45b0dbba7e42
imalkov82 <igor.malkov82@gmail.com>
2015-11-20 13:30:16 +0200

    woking version

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pnd
 import itertools
 from argparse import ArgumentParser
 import os
+import numpy as np
 
 map_rule = {
     'natalia': lambda x: False,
@@ -11,24 +12,28 @@ map_rule = {
     'marina': lambda x: False
 }
 
-index = pnd.MultiIndex.from_arrays([['sunday','sunday','sunday','sunday',
-                                    'monday', 'monday','monday', 'monday',
-                                    'tuesday', 'tuesday','tuesday', 'tuesday',
-                                    'wednesday','wednesday','wednesday','wednesday',
-                                    'thursday','thursday','thursday','thursday',
-                                    'friday','friday'],
-                                   ['morning', 'morning','evening', 'evening',
-                                    'morning', 'morning','evening', 'evening',
-                                    'morning', 'morning','evening', 'evening',
-                                    'morning', 'morning','evening', 'evening',
-                                    'morning', 'morning','evening', 'evening',
-                                    'morning', 'morning'],
-                                   ['employee 1','employee 2', 'employee 1','employee 2',
-                                    'employee 1','employee 2', 'employee 1','employee 2',
-                                    'employee 1','employee 2', 'employee 1','employee 2',
-                                    'employee 1','employee 2', 'employee 1','employee 2',
-                                    'employee 1','employee 2', 'employee 1','employee 2',
-                                    'employee 1','employee 2']])
+index = list(itertools.chain.from_iterable([['morning'] * 2  if i % 2 == 0  else ['evening'] * 2 for i in range(11)]))
+final_index = pnd.MultiIndex.from_arrays([
+    ['sunday','sunday','sunday','sunday',
+    'monday', 'monday','monday', 'monday',
+    'tuesday', 'tuesday','tuesday', 'tuesday',
+    'wednesday','wednesday','wednesday','wednesday',
+    'thursday','thursday','thursday','thursday',
+    'friday','friday'],
+   ['morning', 'morning','evening', 'evening',
+    'morning', 'morning','evening', 'evening',
+    'morning', 'morning','evening', 'evening',
+    'morning', 'morning','evening', 'evening',
+    'morning', 'morning','evening', 'evening',
+    'morning', 'morning']
+   # ['employee 1','employee 2', 'employee 1','employee 2',
+   #  'employee 1','employee 2', 'employee 1','employee 2',
+   #  'employee 1','employee 2', 'employee 1','employee 2',
+   #  'employee 1','employee 2', 'employee 1','employee 2',
+   #  'employee 1','employee 2', 'employee 1','employee 2',
+   #  'employee 1','employee 2']
+   ])
+
 count_success = 0
 
 def genpairs(l):
@@ -50,17 +55,25 @@ def is_consistent(solution, n):
                    for name in solution[n])
     return res
 
-def sidur_exec(solution, workers, pnd_data_frame, min_num, n):
+def sidur_exec(solution, wrks_tup, dfres, min_num, n):
     if len(solution) == n:
-        s = pnd.Series(data=list(itertools.chain.from_iterable(solution)), index= index)
+        data = list(itertools.chain.from_iterable(solution))
+        wrks_num = len(set(data))
+        s = pnd.Series(data=data, index= index)
         if s.value_counts().argmax() == 'natalia' and s.value_counts().std() <= 1:
             if s.value_counts()['natalia'] >= min_num:
-                pnd_data_frame['var_{0}'.format(pnd_data_frame.shape[1] + 1)] = s
+                morning_vals = s['morning'].value_counts()
+                evening_vals = s['evening'].value_counts()
+                if len(morning_vals) == wrks_num and len(evening_vals) == wrks_num:
+                    # if morning_vals.std() < 1 and evening_vals.std() < 1:
+                    #     print('morning std = {0}, evening std = {1}'.format(morning_vals.std(),
+                    #                                                         evening_vals.std()))
+                    dfres['var_{0}'.format(dfres.shape[1] + 1)] = s
     else:
-        for tup in workers:
+        for tup in wrks_tup:
             solution[n] = tup
             if is_consistent(solution, n):
-                sidur_exec(solution, workers, pnd_data_frame, min_num, n + 1)
+                sidur_exec(solution, wrks_tup, dfres, min_num, n + 1)
 
 def update_rules():
     global map_rule
@@ -69,18 +82,26 @@ def update_rules():
     map_rule['marina'] = lambda wrkrs, shift: 'natalia' not in wrkrs
     map_rule['sofia'] = lambda wrkrs, shift: (shift != 2 * 1 + 1) and (shift != 2 * 3 + 1)
 
+def analise_s(s):
+    mstd = s['morning'].value_counts().std()
+    estd = s['evening'].value_counts().std()
+    return pnd.Series([mstd, estd, np.average([mstd,estd])], index=['morning_std', 'evening_std', 'avg'])
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("-n", dest="min_num", help="configuration file for exucution states")
     kvargs = parser.parse_args()
-
     wrk_week = list(range(5 * 2 + 1))
     wrk_names = ['natalia', 'sofia', 'jihan', 'marina']
     update_rules()
     pnd_data_frame = pnd.DataFrame(data=None)
     sidur_exec(wrk_week, genpairs(wrk_names),pnd_data_frame, int(kvargs.min_num), 0)
-    # pnd_data_frame.head()
+
     print(pnd_data_frame.shape)
-    path = '/home/imalkov/Documents/pycharm_workspace/sidur/shifts_{0}_min{1}.csv'.format(os.getpid(),kvargs.min_num)
+    path = '/home/imalkov/Documents/pycharm_workspace/sidur/shifts_res.csv'
+    # path = '/home/imalkov/Documents/pycharm_workspace/sidur/shifts_{0}_min{1}.csv'.format(os.getpid(),kvargs.min_num)
+    stdev_shifs = pnd_data_frame.apply(analise_s).T
+
     print('saving to: {0}'.format(path))
-    pnd_data_frame.to_csv(path)
+    final_data = pnd_data_frame[stdev_shifs.sort_index(by='avg').head(10).index]
+    final_data.to_csv(path)
